757b1c0742e214258a4e48099f6ba295cb51ddb1
vivek <blackarck@gmail.com>
2020-01-23 22:00:44 +0530

    everything working fine to split now Signed-off-by: vivek <blackarck@gmail.com>

--- routes/client.js
+++ routes/client.js
@@ -214,6 +214,8 @@ router.get('/clientid/:clientid', (req, res) => {
     res.send('Input param are ' + req.params.clientid, );
     //pass url as /api/clientid/:clientid/vivek -> route param
     //for query param use optional input res.query.id
+
+
 });
 
 
